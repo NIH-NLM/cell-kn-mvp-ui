a05696b755183e12ab24653abd53dc6efb830894
spearw <spearw@gmail.com>
2024-11-18 14:27:06 -0500

    Update for ArangoDB auth

--- arango_api/db.py
+++ arango_api/db.py
@@ -1,15 +1,17 @@
+import environ
 from arango import ArangoClient
+
+
+# Load db info from .env file
+env = environ.Env()
+environ.Env.read_env()
+
+# Retrieve ArangoDB credentials from the environment
+ARANGO_DB_HOST = env('ARANGO_DB_HOST')
+ARANGO_DB_NAME = env('ARANGO_DB_NAME')
+ARANGO_DB_USER = env('ARANGO_DB_USER')
+ARANGO_DB_PASSWORD = env('ARANGO_DB_PASSWORD')
 
 # Configure the connection
 client = ArangoClient()
-
-# Replace these with your ArangoDB connection details
-DB_HOST = 'http://127.0.0.1:8528'
-##TODO: Generalize DB name?
-DB_NAME = 'BioPortal'
-# No auth currently
-# USERNAME = ''
-# PASSWORD = ''
-
-# Connect to the database
-db = client.db(DB_NAME)
+db = client.db(ARANGO_DB_NAME, username=ARANGO_DB_USER, password=ARANGO_DB_PASSWORD)
