fe52fcd5dfbab55c17dcd9d14d49b4fcb3f96da3
spearw <spearw@gmail.com>
2025-02-12 10:12:02 -0500

    Add BrowseBox tests

--- react/src/components/BrowseBox/BrowseBox.test.js
+++ react/src/components/BrowseBox/BrowseBox.test.js
@@ -1,7 +1,65 @@
-import BrowseBox from './BrowseBox';
+import { render, screen, waitFor } from "@testing-library/react";
+import BrowseBox from "./BrowseBox";
+import * as utils from "../Utils/Utils";
+import { MemoryRouter } from "react-router-dom";
 
-describe('BrowseBox', () => {
-    it("Should have tests", () => {
-        // TODO: Finish testing
-    });;
+// Mocking the fetchCollections and parseCollections functions
+jest.mock("../Utils/Utils", () => ({
+  fetchCollections: jest.fn(),
+  parseCollections: jest.fn(),
+}));
+
+describe("BrowseBox", () => {
+  beforeEach(() => {
+    // Mock Utils
+    const mockFetchedData = ["Collection 1", "Collection 2", "Collection 3"];
+    utils.fetchCollections.mockResolvedValue(mockFetchedData);
+    utils.parseCollections.mockImplementation((data) => data);
+    // Render the BrowseBox with a Router wrapper and currentCollection prop
+    render(
+      <MemoryRouter>
+        <BrowseBox currentCollection="Collection 2" />
+      </MemoryRouter>,
+    );
+  });
+  it("should render collections", async () => {
+    // Wait for collections to load
+    await waitFor(() => {
+      expect(screen.getByText("Collection 1")).toBeInTheDocument();
+      expect(screen.getByText("Collection 2")).toBeInTheDocument();
+      expect(screen.getByText("Collection 3")).toBeInTheDocument();
+    });
+  });
+  it("should highlight the active collection", async () => {
+    // Check
+    await waitFor(() => {
+      expect(screen.getByText("Collection 1").closest("a")).not.toHaveClass(
+        "active",
+      );
+      expect(screen.getByText("Collection 2").closest("a")).toHaveClass(
+        "active",
+      );
+      expect(screen.getByText("Collection 3").closest("a")).not.toHaveClass(
+        "active",
+      );
+    });
+  });
+
+  it("should render links to each collection with correct href", async () => {
+    // Check the href attribute for each link
+    await waitFor(() => {
+      expect(screen.getByText("Collection 1").closest("a")).toHaveAttribute(
+        "href",
+        "/browse/Collection 1",
+      );
+      expect(screen.getByText("Collection 2").closest("a")).toHaveAttribute(
+        "href",
+        "/browse/Collection 2",
+      );
+      expect(screen.getByText("Collection 3").closest("a")).toHaveAttribute(
+        "href",
+        "/browse/Collection 3",
+      );
+    });
+  });
 });
