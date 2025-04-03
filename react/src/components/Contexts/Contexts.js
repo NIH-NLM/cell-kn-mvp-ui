57f724e19f4ee84f6e11acf256cd58a5163cd8d8
spearw <spearw@gmail.com>
2025-04-03 10:29:52 -0400

    Update Contexts test

--- react/src/components/Contexts/Contexts.js
+++ react/src/components/Contexts/Contexts.js
@@ -3,4 +3,4 @@ import { createContext } from "react";
 /* TODO: review if other variables should be context based */
 export const GraphNameContext = createContext("KN-v2.0");
 export const DbNameContext = createContext("Cell-KN");
-export const PrunedCollections = createContext(["NCBITaxon"])
+export const PrunedCollections = createContext(["NCBITaxon"]);
