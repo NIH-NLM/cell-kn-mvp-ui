0992842714e5e856a7e3a8f8ee23844a3f685d38
Ubuntu <ubuntu@ip-172-31-5-97.ec2.internal>
2025-05-26 11:12:22 +0000

    Add documentation and archive old configuration

--- core/sh/build.sh
+++ core/sh/build.sh
@@ -8,13 +8,37 @@ NAME
     build - Build the Cell KN MVP ArangoDB archive
 
 SYNOPSIS
-    build 
+    build [OPTIONS]
 
 DESCRIPTION
-    Build the Cell KN MVP.
+    Build the Cell KN MVP by checking out the specified versions to
+    build, making clean packages, building the ontology graph,
+    fetching external data, building the results and phenotype graphs,
+    archiving the ArangoDB database, and copying the archive to
+    cell-kn-mvp.org. Changes on the current branches are stashed prior
+    to checking out the specified versions, then applied on checking
+    out the current version after the build.
 
 OPTIONS 
+    -o    CELL_KN_ETL_ONTOLOGIES_VERSION
+          Build the specified version of the ontologies graph, if it
+          does not exist
+
+    -O    CELL_KN_ETL_ONTOLOGIES_VERSION
+          Force -o
+
+    -r    CELL_KN_ETL_RESULTS_VERSION
+          Build the specified version of the results and phenotype
+          graph, if they do not exist
+
+    -R    CELL_KN_ETL_RESULTS_VERSION
+          Force -r
+
     -h    Help
+
+    -e    Exit immediately if a command returns a non-zero status
+
+    -x    Print a trace of simple commands
 
 EOF
 }
@@ -26,19 +50,23 @@ run_results=0
 force_results=0
 make_archive=0
 force_archive=0
-while getopts ":oOrRaAh" opt; do
+while getopts ":o:O:r:R:aAhex" opt; do
     case $opt in
         o)
             run_ontology=1
+	    CELL_KN_ETL_ONTOLOGIES_VERSION=${OPTARG}
             ;;
         O)
             force_ontology=1
+	    CELL_KN_ETL_ONTOLOGIES_VERSION=${OPTARG}
             ;;
         r)
             run_results=1
+	    CELL_KN_ETL_RESULTS_VERSION=${OPTARG}
             ;;
         R)
             force_results=1
+	    CELL_KN_ETL_RESULTS_VERSION=${OPTARG}
             ;;
         a)
             make_archive=1
@@ -49,6 +77,12 @@ while getopts ":oOrRaAh" opt; do
 	h)
 	    usage
 	    exit 0
+	    ;;
+	e)
+	    set -e
+	    ;;
+	x)
+	    set -x
 	    ;;
 	\?)
 	    echo "Invalid option: -${OPTARG}" >&2
@@ -70,136 +104,122 @@ if [ "$#" -ne 0 ]; then
     exit 1
 fi
 
-# TODO: Set in environment by requiring conf file argument
-CELL_KN_ETL_ONTOLOGIES_VERSION="v0.1.1"
-CELL_KN_ETL_RESULTS_VERSION="v0.3.0"
-
-set -e
-
-# Build ontology graph
-
+# Build ontology graph, if specified
 pushd "../../cell-kn-etl-results/cell-kn-etl-ontologies"
-
 if [ ! -f ".built" ] && [ $run_ontology == 1 ] \
        || [ $force_ontology == 1 ]; then
 
+    # Stop ArangoDB and remove the database
     pushd "src/main/shell"
     ./stop-arangodb.sh
     popd
-
     pushd "data"
     rm -rf arangodb
     popd
-
     pushd "src/main/shell"
     ./start-arangodb.sh
     popd
 
-    current_branch=$(git branch --show-current)
+    # Stash changes, set the current branch, checkout the version to
+    # build
     git stash
+    current_branch=$(git branch --show-current)
     git checkout $CELL_KN_ETL_ONTOLOGIES_VERSION
 
+    # Make a clean package, then build the ontology graph
     mvn clean package -DskipTests
-
     classpath="target/cell-kn-etl-ontologies-1.0.jar"
-
     java -cp $classpath gov.nih.nlm.OntologyGraphBuilder
 
+    # Checkout the current branch, and apply the stash so that changes
+    # are not lost
     git checkout $current_branch
     git stash apply
 
+    # Log the build
     log_message="Built cell-kn-etl-ontologies"
     log_message+=" using $CELL_KN_ETL_ONTOLOGIES_VERSION"
     log_message+=" on $(date)"
-
     echo $log_message > ".built"
 
 fi
-
 popd
 
-# Build results and phenotype graphs
-
+# Build results and phenotype graphs, if specified
 pushd "../../cell-kn-etl-results"
-
 if [ ! -f ".built" ] && [ $run_results == 1 ] \
        || [ $force_results == 1 ]; then
 
+    # Stash changes, set the current branch, checkout the version to
+    # build
     current_branch=$(git branch --show-current)
     git stash
     git checkout $CELL_KN_ETL_RESULTS_VERSION
 
+    # Activate the Python environment, fetch external data, and write
+    # all tuples. Note that the local .zshenv contains E-Utilities
+    # credentials
     . .venv/bin/activate
-
     pushd src/main/python
-
     . .zshenv
-
     python ExternalApiResultsFetcher.py
-
     python NSForestResultsTupleWriter.py
     python AuthorToClResultsTupleWriter.py
     python ExternalApiResultsTupleWriter.py
+    # TODO: Uncomment when complete
     # python AnnotationResultsTupleWriter.py
-
     popd
 
+    # Make a clean package, then build the results and phenotype
+    # graphs
     mvn clean package -DskipTests
-
     classpath="target/cell-kn-etl-results-1.0.jar"
     classpath+=":cell-kn-etl-ontologies/target/cell-kn-etl-ontologies-1.0.jar"
-
     java -cp $classpath gov.nih.nlm.ResultsGraphBuilder
     java -cp $classpath gov.nih.nlm.PhenotypeGraphBuilder
 
+    # Create all analyzers and views, then deactivate the Python
+    # environment
     pushd src/main/python
-
     python CellKnSchemaUtilities.py
-
     popd
-
     deactivate
 
+    # Checkout the current branch, and apply the stash so that changes
+    # are not lost
     git checkout $current_branch
     git stash apply
 
+    # Log the build
     log_message="Built cell-kn-etl-results"
     log_message+=" using $CELL_KN_ETL_RESULTS_VERSION"
     log_message+=" on $(date)"
-
     echo $log_message > ".built"
 
 fi
-
 popd
 
-# Make ArangoDB archive
-
+# Make ArangoDB archive, if specified
 pushd "../../cell-kn-etl-results/cell-kn-etl-ontologies"
-
 if [ ! -f ".archived" ] && [ $make_archive == 1 ] \
        || [ $force_archive == 1 ]; then
 
+    # Make the archive, and copy it to cell-kn-mvp.org
     pushd data
-
     archive="arangodb"
     archive+="-$CELL_KN_ETL_ONTOLOGIES_VERSION"
     archive+="-$CELL_KN_ETL_RESULTS_VERSION"
     archive+="-$(date "+%Y-%m-%d").tar.gz"
-
     tar -czvf $archive arangodb
-
     scp $archive mvp:~
+    popd
 
+    # Log the archive
     log_message="Archived ArangoDB using"
     log_message+=" $CELL_KN_ETL_ONTOLOGIES_VERSION"
     log_message+=" and $CELL_KN_ETL_RESULTS_VERSION"
     log_message+=" on $(date)"
-
     echo $log_message > ".archived"
 
-    popd
-
 fi
-
 popd
--- core/sh/deploy.sh
+++ core/sh/deploy.sh
@@ -1,60 +1,121 @@
+# Print usage
+usage() {
+    cat << EOF
+
+NAME
+    deploy - Deploy Cell KN MVP
+
+SYNOPSIS
+    deploy [OPTIONS]
+
+DESCRIPTION
+    Deploy the Cell KN MVP by deploying each configuration in the conf
+    directory. All sites are first disabled, then the configured
+    ArangoDB archive is extracted, renamed, and symbolically linked
+    using the specified port. The site configuration template is
+    updated, then installed into the Apache sites-available directory,
+    and the site enabled.
+
+OPTIONS 
+    -h    Help
+
+    -e    Exit immediately if a command returns a non-zero status
+
+    -x    Print a trace of simple commands
+
+EOF
+}
+
+# Parse command line options
+while getopts ":hex" opt; do
+    case $opt in
+	h)
+	    usage
+	    exit 0
+	    ;;
+        e)
+            set -e
+            ;;
+        x)
+            set -x
+            ;;
+	\?)
+	    echo "Invalid option: -${OPTARG}" >&2
+	    usage
+	    exit 1
+	    ;;
+	\:)
+	    echo "Option -${OPTARG} requires an argument" >&2
+	    usage
+	    exit 1
+	    ;;
+    esac
+done
+
+# Parse command line arguments
+shift `expr ${OPTIND} - 1`
+if [ "$#" -ne 0 ]; then
+    echo "No arguments required"
+    exit 1
+fi
+
 DOMAIN="cell-kn-mvp.org"
 
-set -x
+# Disable all enabled sites
+sites=$(ls /etc/apache2/sites-enabled)
+for site in $sites; do
+    sudo a2dissite $site
+    sleep 1
+done
 
+# Stop all ArangoDB containers
+ports=$(docker ps | grep arangodb | cut -d "-" -f 4)
+for port in $ports; do
+    ARANGO_DB_PORT=$port ../../arango_api/sh/stop-arangodb.sh
+done
+
+# List the configurations, and ensure only one default exists
 confs=$(ls conf/* | grep -v [~#])
-
 if [ $(grep "IS_DEFAULT=1" $(ls conf/* | grep -v [~#]) | wc -l) != 1 ]; then
     echo "One site, and only one site, must be default"
     exit 0
 fi
-
 for conf in $confs; do
 
+    # Source the configuration to define CELL_KN_MVP_VERSION,
+    # ARANGO_DB_FILE, ARANGO_DB_PORT, SUBDOMAIN, IS_DEFAULT,
+    # SERVER_ADMIN
     . $conf
 
+    # Extract, rename, and symbolically link the ArangoDB archive
     pushd ~
-    
-    ./stop-arangodb.sh
-    
     arango_db_file=$(echo $ARANGO_DB_FILE | sed s/.tar.gz/-$CELL_KN_MVP_VERSION/)
     arango_db_link=arangodb-$ARANGO_DB_PORT
-
     rm -rf $arango_db_file
     sudo rm -rf $arango_db_link
-
     tar -zxvf $ARANGO_DB_FILE
     mv arangodb $arango_db_file
     sudo ln -sf $arango_db_file $arango_db_link
-
     ./start-arangodb.sh
-
     popd
     
+    # Update, install, and enable the Apache site configuration
     SITE=$CELL_KN_MVP_VERSION-cell-kn-mvp.conf
-    
-    sudo a2dissite $SITE
-
     cat 000-default.conf | \
 	sed s/{version}/$CELL_KN_MVP_VERSION/ | \
 	sed s/{subdomain}/$SUBDOMAIN/ | \
 	sed s/{server_admin}/$SERVER_ADMIN/ \
 	    > $SITE
-
     sed -i \
 	"s%.*ARANGO_DB_HOST.*%ARANGO_DB_HOST=http://127.0.0.1:$ARANGO_DB_PORT%" \
 	~/springbok-cell-kn-mvp-$CELL_KN_MVP_VERSION/arango_api/.env
-
     if [ $IS_DEFAULT == 1 ]; then
-	sudo sed -i "s/.*ServerName.*/    ServerName $DOMAIN/" $SITE
+	sed -i "s/.*ServerName.*/    ServerName $DOMAIN/" $SITE
     fi
-
     sudo cp $SITE /etc/apache2/sites-available
-
     rm $SITE
-    
     sudo a2ensite $SITE
-
     sudo systemctl reload apache2
+    sleep 1
 
 done
