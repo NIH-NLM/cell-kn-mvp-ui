f995979c83dcdf1323bc1aad1ccc8453f4197e83
spearw <spearw@gmail.com>
2025-05-29 15:28:28 -0400

    Update color and styling for sunburst

--- react/src/pages/SunburstPage/SunburstPage.js
+++ react/src/pages/SunburstPage/SunburstPage.js
@@ -2,9 +2,17 @@ import Sunburst from "../../components/Sunburst/Sunburst";
 
 const SunburstPage = () => {
   return (
-    <div className="exploration-page">
-      <div className="sunburst-search-container">
-        <Sunburst />
+    <div className="visualization-page-layout">
+      <div className="visualization-content-box">
+        <h1 className="visualization-page-title">Browse the Database</h1>
+        <div className="sunburst-visualization-container">
+          <Sunburst />
+        </div>
+        <p className="visualization-description">
+          This sunburst chart visualizes the structure and distribution of data
+          entities within this database. Each arc represents a vertex with its
+          size proportional to the number of downstream vertices.
+        </p>
       </div>
     </div>
   );
