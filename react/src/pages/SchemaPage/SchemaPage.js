e7a252191937985d66f56286d64096d02b5fecc2
spearw <spearw@gmail.com>
2025-05-29 16:23:32 -0400

    Update schema page

--- react/src/pages/SchemaPage/SchemaPage.js
+++ react/src/pages/SchemaPage/SchemaPage.js
@@ -3,8 +3,17 @@ import schemaImage from "../../assets/schema.png";
 
 const SchemaPage = () => {
   return (
-    <div>
-      <img src={schemaImage} alt="Schema" className="schema-image" />
+    <div className="content-page-layout">
+      <div className="content-box schema-display-box">
+        <h1 className="content-page-title">MVP Schema</h1>
+        <div className="image-container schema-image-container">
+          <img
+            src={schemaImage}
+            alt="Database Schema Diagram"
+            className="responsive-image schema-image-actual"
+          />
+        </div>
+      </div>
     </div>
   );
 };
