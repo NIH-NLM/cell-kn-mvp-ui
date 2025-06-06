7ccec1b3b0c3b5bdeec5e09f217f5c092f1aba74
Ubuntu <ubuntu@ip-172-31-5-97.ec2.internal>
2025-06-06 00:18:34 +0000

    Clean up

--- core/sh/deploy.sh
+++ core/sh/deploy.sh
@@ -158,7 +158,7 @@ popd
 # Update, install, and enable the Apache site configuration
 cat 000-default.conf | \
     sed s/{subdomain}/$SUBDOMAIN/ | \
-    sed s/{server_admin}/$SERVER_ADMIN/ \
+    sed s/{server_admin}/$SERVER_ADMIN/ | \
     sed s/{cell_kn_mvp_ui_version}/$CELL_KN_MVP_UI_VERSION/ | \
 	> $site
 if [ $IS_DEFAULT == 1 ]; then
